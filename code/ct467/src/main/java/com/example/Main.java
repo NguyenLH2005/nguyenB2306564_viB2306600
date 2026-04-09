90cf89511051f5fe7e03ac89586fbaeda2da9bc6
DuongKhoaiLang <duongking001@gmail.com>
2026-04-09 09:54:25 +0700

    phân luồng hoạt động

--- code/ct467/src/main/java/com/example/Main.java
+++ code/ct467/src/main/java/com/example/Main.java
@@ -1,19 +1,22 @@
 package com.example;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import com.example.db.DBConnection;
+
+// Luồng hoạt dộng Main -> service -> Repository -> DB
+// Main chạy console
+// service là nơi chứa logic nghiệp vụ
+// Repository là nơi chứa logic truy cập dữ liệu
+// model đại diện bảng trong cơ sở dữ liệu (các thuộc tính, getter, setter, constructor,toString)
 
 public class Main {
- public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/",
-                "root",
-                ""
-            );
+            Connection conn = DBConnection.getConnection();
             System.out.println("Kết nối thành công!");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
