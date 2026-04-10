c1d3db54e4602ee481a7ccf442f8aeaffe0eed74
DuongKhoaiLang <duongking001@gmail.com>
2026-04-10 11:48:20 +0700

    cập nhật pptx transaction

--- code/ct467/src/main/java/com/example/repository/DiemRepository.java
+++ code/ct467/src/main/java/com/example/repository/DiemRepository.java
@@ -6,7 +6,6 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class DiemRepository {
 
